fd53e0f4be39788befac352eafcde1fa5e23c16f
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-04-13 04:01:52 -0400

    Fixing Task 3

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -17,22 +17,23 @@ const TourCard = ({ id, name, info, image, price, onRemove }) => {
       <div className="tour-info">
         <div className="tour-header">
           {/* Display the tour name and price */}
-                <h2>{name}</h2>
-                <h3 className="tour-price">${price}</h3>
-              </div>
-                {/* Show the tour description. If readMore is false, show only the first 150 characters followed by '...'. Otherwise, show the full description */}
-                <p>
-                {readMore ? info : `${info.substring(0, 150)}...`}
-
-                {/* Button to toggle between showing the full description or a shortened version */}
-            <button className="read-more" onClick={() => setReadMore(!readMore)}>
-              {readMore ? "Show Less" : "Read More"}
-            </button>
-          </p>
-          {/* Button to remove the tour from the list */}
-          <button className="not-interested" onClick={() => onRemove(id)}>
-            Not Interested
+          <h2>{name}</h2>
+          <h3 className="tour-price">${price}</h3>
+        </div>
+        
+        {/* Show the tour description. If readMore is false, show only the first 150 characters followed by '...'. Otherwise, show the full description */}
+        <p>
+          {readMore ? info : `${info.substring(0, 150)}...`}
+          {/* Button to toggle between showing the full description or a shortened version */}
+          <button className="read-more" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
           </button>
+        </p>
+        
+        {/* Button to remove the tour from the list */}
+        <button className="not-interested" onClick={() => onRemove(id)}>
+          Not Interested
+        </button>
       </div>
     </article>
   );
